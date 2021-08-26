1fdafa940a3e6fcf0f4aaa21dde0068a93419d1a
MajorHorse <mylove103@foxmail.com>
2021-08-26 23:30:04 +0800

    update

--- spring-demo-security/src/main/java/com/spring/demo/security/service/MyUserDetailsServiceImpl.java
+++ spring-demo-security/src/main/java/com/spring/demo/security/service/MyUserDetailsServiceImpl.java
@@ -1,17 +1,28 @@
 package com.spring.demo.security.service;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author Major Tom
  * @date 2021/8/25 11:55
  * @description 自定义实现类
  */
+@Service("userDetailsService")
 public class MyUserDetailsServiceImpl implements UserDetailsService {
+
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+        List<GrantedAuthority> auths = AuthorityUtils
+                .commaSeparatedStringToAuthorityList("role");
+        return new User("mhg", new BCryptPasswordEncoder().encode("1234"), auths);
     }
 }
